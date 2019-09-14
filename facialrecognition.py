f3a1c727f304732614d463a70d2e9758ad701615
Darren Li <d338li@edu.uwaterloo.ca>
2019-09-14 19:42:31 -0400

    working prototype give to michael

--- facialrecognition.py
+++ facialrecognition.py
@@ -13,14 +13,15 @@ load_dotenv()
 key = os.environ["FACE_KEY"]
 endpoint = os.environ["FACE_ENDPOINT"]
 
-print()
-print(endpoint)
-print()
+meme_path = os.path.dirname(os.path.realpath(__file__) + "/Memes")
+print(meme_path)
+for r, d, f in os.walk(meme_path):
+    print (f)
 
 face_client = FaceClient(endpoint, CognitiveServicesCredentials(key))
 
 # Detect a face in an image that contains a single face
-single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+single_face_image_url = "https://scontent.fyyz1-1.fna.fbcdn.net/v/t1.15752-9/70888134_2522150378019046_2182028800106168320_n.png?_nc_cat=111&_nc_oc=AQl7S4cmsiXW0dJpeqZDfE6ZAe0azVJ0q3ULjB7iN2zoZx3p4vq_RpO-dHD1-okLlvw&_nc_ht=scontent.fyyz1-1.fna&oh=64c57e1b621411f92ea903ae527d62c9&oe=5DF2D71A"
 single_image_name = os.path.basename(single_face_image_url)
 
 detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
@@ -51,3 +52,9 @@ else:
 
 # # Display the image in the users default image browser.
 # img.show()
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
